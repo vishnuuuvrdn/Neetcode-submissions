10e500072f3040ce4e1329ac5c219f5a8dbe188b
Vishnu Vardhan <94221639+vishnuuuvrdn@users.noreply.github.com>
2026-07-07 08:00:48 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums[0] != 0) return 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] - nums[i-1] != 1){
+                return nums[i-1]+1;
+            }
+        }
+        return nums.size();
+    }
+};
