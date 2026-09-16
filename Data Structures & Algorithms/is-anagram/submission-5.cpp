c669c49b23d6dac43a2a2ee1268b2a8c435102dc
Vishnu Vardhan <94221639+vishnuuuvrdn@users.noreply.github.com>
2026-09-16 15:17:48 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.size() > t.size() || t.size() > s.size()){
+            return false;
+        }
+
+        map<char, int> mp;
+
+        for(char c : s){
+            mp[c]++;
+        }
+
+        for(char c : t){
+            if(!mp[c]) return false;
+            
+            mp[c]--;
+        }
+
+        return true;
+    }
+};
