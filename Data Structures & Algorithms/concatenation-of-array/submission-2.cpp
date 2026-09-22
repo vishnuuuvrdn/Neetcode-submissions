818572be6230b2be0b5efe7259997c2ddcb1bcdf
Vishnu Vardhan <94221639+vishnuuuvrdn@users.noreply.github.com>
2026-09-22 15:40:42 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> res;
+        for(int num : nums){
+            res.push_back(num);
+        }
+
+        for(int num : nums){
+            res.push_back(num);
+        }
+
+        return res;
+    }
+};
